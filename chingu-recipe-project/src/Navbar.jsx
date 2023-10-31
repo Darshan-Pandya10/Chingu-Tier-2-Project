c7c34f77e9b357d6cf7879708324882524dc91c7
Darshan Pandya <andyadarshan811@gmail.com>
2023-10-31 23:03:22 +0530

    [changes] in theme button.

--- chingu-recipe-project/src/Navbar.jsx
+++ chingu-recipe-project/src/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
   const {theme , toggleTheme} = themeValue;
 
   return (
-    <div className="navbar w-full h-24 flex justify-between items-center text-white px-4 py-2 ">
+    <div className="navbar w-full h-24 flex justify-between items-center text-white px-2 py-2 ">
       <h2 className="logo left-0 text-white">Foodish Recipes</h2>
       <nav className='w-fit flex right-0 '>
         <NavLink to='/'>Home</NavLink>
